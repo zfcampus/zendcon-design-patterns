818566dde09ac11dfcacf800ecb0569cc12c4246
Nick <neserag95@gmail.com>
2019-03-27 11:39:18 +0200

    It is not an exercise but solution!

--- Prototype/exercise.php
+++ Prototype/exercise.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace ShopingCartFramework {
     class Shop {
         protected $productPrototype;
@@ -9,8 +8,7 @@ namespace ShopingCartFramework {
         public function listProducts(array $codes) {
             $output = [];
             foreach ($codes as $code) {
-                $product = clone $this->productPrototype;
-                $product->initialize($code);
+                // @TODO create an actual $product, and initialize it
                 $output[] = $product->getShopProductCode() . ' - ' . $product->getShopDescription();
             }
             return implode(PHP_EOL, $output);
@@ -22,33 +20,13 @@ namespace ShopingCartFramework {
         public function getShopDescription();
     }
 }
-
 namespace MyCompanyShop {
     use ShopingCartFramework\ProductInterface;
-    class MyShopProduct implements ProductInterface {
-        protected $productService = array();
-        protected $code, $description;
-        public function __construct(\Closure $productService) {
-            $this->productService = $productService;
-        }
-        public function initialize($code) {
-            $this->code = $code;
-            $this->description = call_user_func($this->productService, $code);
-        }
-        public function getShopProductCode() {
-            return $this->code;
-        }
-        public function getShopDescription() {
-            return $this->description;
-        }
-    }
-
+	// @TODO implement MyShopProduct prototype
 }
-
 namespace {
     use ShopingCartFramework\Shop;
     use MyCompanyShop\MyShopProduct;
-
     $mockWebService = function($code) {
         static $data = [
             'BumperSticker1' => 'Cool bumper sticker',
@@ -56,10 +34,7 @@ namespace {
         ];
         return $data[$code];
     };
-
     $shop = new Shop(new MyShopProduct($mockWebService));
-
     $productsToList = ['BumperSticker1', 'CoffeeTableBook5'];
-    header('content-type: plain/text');
     echo $shop->listProducts($productsToList); // simulation of Shopping Cart Listings Page
 }
